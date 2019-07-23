0afdc3e665ae99572b6680761fa0242f1bbaec72
schoeu <schoeu89@gmail.com>
2019-07-23 10:47:30 +0800

    update agent.

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module pslog_agent
+module github.com/schoeu/pslog_agent
 
-go 1.13
+go 1.12
 
 require github.com/urfave/cli v1.20.0
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
+	"time"
 
 	"github.com/urfave/cli"
+)
+
+type config struct {
+	Appid  string
+	Secret string
+	Logdir   string
+
+}
+
+var (
+	//interval = 60000
+	interval = 1000
 )
 
 func main() {
@@ -14,11 +30,34 @@ func main() {
 	app.Version = "1.0.0"
 	app.Name = "pslog_agent"
 	app.Usage = "Agent for ps log"
+
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "config, c",
+			Value: "",
+			Usage: "configuration file path.",
+		},
+	}
+
+	app.Action = defaultAction
+
 	app.Commands = []cli.Command{
 		{
 			Name:    "start",
 			Usage:   "start app on agent.",
 			Action:  startAction,
+		},
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "status fro agent.",
+			Action:  listAction,
+		},
+		{
+			Name:    "stop",
+			Aliases: []string{"l"},
+			Usage:   "stop app on agent.",
+			Action:  stopAction,
 		},
 	}
 
@@ -26,19 +65,81 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func getLog() {
+	defaultLogName := ".psinfo.log"
+	homeDir, err := os.UserHomeDir()
+	errHandler(err)
+	defaultLogFile := path.Join(homeDir, defaultLogName)
+	fmt.Println(defaultLogFile)
+
+}
+
+func getConfig(p string) (config, error) {
+	if !path.IsAbs(p) {
+		p = path.Join(getCwd(), p)
+	}
+
+	c := config{}
+	data, err := ioutil.ReadFile(p)
+	err = json.Unmarshal(data, &c)
+
+	return c, err
+}
+
+func errHandler(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func startAction(c *cli.Context) error {
-	//fmt.Println("added task: ", c.Args())
+	fmt.Println(c.String("config"))
 
 
 	return nil
 }
 
-func getLog() {
-	defaultLogName := ".psinfo.log"
-	homeDir := os.UserHomeDir()
-	defaultLogFile := path.Join(homeDir, defaultLogName)
+func listAction(c *cli.Context) error {
+	fmt.Println("listAction")
+	return nil
+}
 
+func stopAction(c *cli.Context) error {
+	fmt.Println("stopAction")
+	return nil
+}
 
+func defaultAction(c *cli.Context) error {
+	configFile := c.Args().First()
+	ext := path.Ext(configFile)
+	if ext == ".json" {
+		conf, err := getConfig(configFile)
+		errHandler(err)
+
+		// get config data: Appid, Secret, Logdir
+		fmt.Println("defaultAction", conf.Appid)
+	}
+	return nil
+}
+
+func timer(interval int) {
+	d := time.Duration(time.Millisecond * time.Duration(interval))
+	t := time.NewTicker(d)
+	defer t.Stop()
+
+	for {
+		<-t.C
+		getLog()
+	}
+}
+
+func getCwd() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+	return dir
 }
