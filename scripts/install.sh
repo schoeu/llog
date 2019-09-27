be3373531ddeed17f52416b8da74029f2f41dc48
schoeu <schoeu89@gmail.com>
2019-09-27 22:39:52 +0800

    change install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,15 @@ sys_bit=`getconf LONG_BIT`
 if [ "$sys_bit" = "64" ]
 then
   # wget 64bit version.
-  wget http://qiniucdn.schoeu.com/nma_64bit
-  mv nma_64bit nma
+  wget http://qiniucdn.schoeu.com/lla_64bit
+  mv lla_64bit lla
 elif [ "$sys_bit" = "32" ]
 then
   # wget 32bit version.
-  wget http://qiniucdn.schoeu.com/nma_32bit
-  mv nma_32bit nma
+  wget http://qiniucdn.schoeu.com/lla_32bit
+  mv lla_32bit lla
 fi
 
-chmod +x nma
-kill -9 `ps aux | grep nma | head -n 1 | awk '{print $2}'`
-nohup ./nma >> ./nma_nohup.log 2>&1 &
+chmod +x lla
+kill -9 `ps aux | grep lla | head -n 1 | awk '{print $2}'`
+nohup ./lla >> ./lla_nohup.log 2>&1 &
