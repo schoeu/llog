5def80867c9172baf9cffc4ff62c2d0db49c890a
schoeu <schoeu89@gmail.com>
2019-10-18 23:34:01 +0800

    update readme.

--- go.mod
+++ go.mod
@@ -1,13 +1,12 @@
 module github.com/schoeu/llog
 
-go 1.12
+go 1.13
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.7
-	github.com/olivere/elastic/v7 v7.0.7
+	github.com/olivere/elastic/v7 v7.0.8
 	github.com/satori/go.uuid v1.2.0
 	github.com/schoeu/gopsinfo v1.2.8
 	github.com/urfave/cli v1.22.1
-	gopkg.in/yaml.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.2.4
 )
--- scripts/builder.sh
+++ scripts/builder.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # linux 64bit
-GOOS=linux GOARCH=amd64 go build -ldflags '-w -s' -o ./bin/lla_64bit
-upx -9 ./bin/lla_64bit
+GOOS=linux GOARCH=amd64 go build -ldflags '-w -s' -o ./bin/llog_64bit
+upx -9 ./bin/llog_64bit
 
 # linux 32bit
-GOOS=linux GOARCH=386 go build -ldflags '-w -s' -o ./bin/lla_32bit
-upx -9 ./bin/lla_32bit
+GOOS=linux GOARCH=386 go build -ldflags '-w -s' -o ./bin/llog_32bit
+upx -9 ./bin/llog_32bit
 
 # windows 64bit，  windows server暂不考虑
-#GOOS=windows GOARCH=amd64 go build -ldflags '-w -s' -o ./bin/lla_64bit.exe
-#upx -9 ./bin/lla_64bit.exe
+#GOOS=windows GOARCH=amd64 go build -ldflags '-w -s' -o ./bin/llog_64bit.exe
+#upx -9 ./bin/llog_64bit.exe
 
 # windows 32bit，  windows server暂不考虑
-#GOOS=windows GOARCH=386 go build -ldflags '-w -s' -o ./bin/lla_32bit.exe
-#upx -9 ./bin/lla_32bit.exe
+#GOOS=windows GOARCH=386 go build -ldflags '-w -s' -o ./bin/llog_32bit.exe
+#upx -9 ./bin/llog_32bit.exe
 
 # Mac OS X 64bit，线下测试使用
-GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s' -o ./bin/lla_mac
-upx -9 ./bin/lla_mac
+GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s' -o ./bin/llog_mac
+upx -9 ./bin/llog_mac
